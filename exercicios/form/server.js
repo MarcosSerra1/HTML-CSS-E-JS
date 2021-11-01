b42fd7f9320c0dbce245672db2362c58a4b1eb77
Marcos Serra <marcosserra133@gmail.com>
2021-11-01 17:39:47 -0300

    novas aulas 1.1

--- exercicios/form/server.js
+++ exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true}))
 
 app.post('/usuarios',(req, resp) =>{
     console.log(req.body)
-    resp.send('<h2>Parabens</h2>')
+    resp.send('<h2>Parabens. Usuario Incluido com Sucesso!</h2>')
+})
+
+app.post('/usuarios/:id',(req, resp) =>{
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h2>Parabens. Usuario alterado com Sucesso!</h2>')
 })
 
 app.listen(3003)
